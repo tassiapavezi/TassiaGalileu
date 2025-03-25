58b2849e1b7c5f4ff915eeb9c1190ce42337b7c5
tassiapavezi <tassiapacri1@gmail.com>
2025-03-25 15:41:20 -0300

    Add files via upload

--- Alfanumericos.py
+++ Alfanumericos.py
@@ -1,2 +1,2 @@
-senha = "Tassia123"
-print(type(senha))
+senha = "Tassia123" #estou criando uma variavel chamada senha e atribuindo o tipo de dado STR igual a Tassia123.
+print(type(senha)) #quero que imprima na tela o tipo de dado da variavel
