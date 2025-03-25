2f95b68fb9e0b2815b9c8a93a3077490108050a0
tassiapavezi <tassiapacri1@gmail.com>
2025-03-25 16:09:32 -0300

    Add files via upload

---
+++ Booleanos.py
@@ -0,0 +1,8 @@
+Senha = "Tassia123" #Atribuir a str Tassia123 como o dado armazenado na variavel senha
+tentativa_senha = input("digite a sua senha:") 
+"""criei uma varariavel e usando a funcao input, pedi para o usuario informar a senha""" 
+
+print (Senha == tentativa_senha) 
+"""pedi para imprimir o resultado da comparacao entre senha e a tentaiva do usuario 
+e vai retornar um resultado Booleano"""
+""" se a comparacao for igaul o resultado sera TRUE, se nao sera, FALSE"""
