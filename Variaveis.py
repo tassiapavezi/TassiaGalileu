e816975da7e96feef2de4d4b725bbd9462859545
tassiapavezi <tassiapacri1@gmail.com>
2025-03-25 17:05:52 -0300

    Add files via upload

---
+++ Variaveis.py
@@ -0,0 +1,7 @@
+#criar variavel nome
+#criar um programa onde peca para o usuario informar um nome.
+#logo me seguida imprimir a info dessa variavel e o seu tipo.
+
+Nome = input ("digite o nome que sera atribuido:")
+print(Nome)
+print(type(Nome))
