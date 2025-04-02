4dbef82f0050fd38cc2153f906be247f27d2dece
tassiapavezi <tassiapacri1@gmail.com>
2025-04-02 15:32:16 -0300

    Add files via upload

---
+++ operadores 2.py
@@ -0,0 +1,6 @@
+variavel_numero_1 = int(input ("digite o primeiro numero:"))
+variavel_numero_2 = int(input ("digite o segundo numero:"))
+print(f'a soma dos dois numeros e de: {variavel_numero_1 + variavel_numero_2}')
+
+#realizando a operacaod e subitracao
+print(f'a subitracao dos dois numeros e de: {variavel_numero_1 - variavel_numero_2}')
