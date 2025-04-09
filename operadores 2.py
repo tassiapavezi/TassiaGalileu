14bc1f7d2fc7dd420c714245ef04d2da94abb6a5
tassiapavezi <tassiapacri1@gmail.com>
2025-04-09 15:09:33 -0300

    Add files via upload

--- operadores 2.py
+++ operadores 2.py
@@ -4,3 +4,29 @@ print(f'a soma dos dois numeros e de: {variavel_numero_1 + variavel_numero_2}')
 
 #realizando a operacaod e subitracao
 print(f'a subitracao dos dois numeros e de: {variavel_numero_1 - variavel_numero_2}')
+
+#Realizando a operacao de multiplicacao
+print(f' a multiplicacao dos numeros e de: {variavel_numero_1 * variavel_numero_2}')
+
+#criar variavel multiplicacao
+multiplicacao = variavel_numero_1 * variavel_numero_2
+print (multiplicacao) #imprimir o valor armazenado na multiplicacao
+
+print (f'a multiplicacao dos dois numeros e de: {multiplicacao}')
+
+#Criar uma variavel chamada exponencial resultado 
+# atribua o valor entre variavel 1 e variavel 2
+#Logo em seguida, imprima exponencial resultado utilizando o F-SyntaxWarning
+
+exponencialresultado = variavel_numero_1 ** variavel_numero_2
+
+print (f' o exponencial resultado da v1 exponecial v2: {exponencialresultado}')
+
+#agora vou usar  o operador divisao
+divisao = variavel_numero_1 / variavel_numero_2
+print (f' o resultado da divisao de v1 exponecial v2: {divisao}')
+
+#Em Python o % chama RESTO e nao faz o percentual
+resto = variavel_numero_1 % variavel_numero_2
+print (f' o resto da divisao de v1 com v2: {resto}')
+
