70684b7d7419ad436da48ce6716512aa1e296bda
tassiapavezi <tassiapacri1@gmail.com>
2025-04-30 14:16:09 -0300

    Add files via upload

---
+++ DesafioTassia.py
@@ -0,0 +1,9 @@
+"""Objetivo: Praticar input(), print(), variáveis e subtração.
+
+Descrição:
+Peça ao aluno que crie um programa que pergunte o ano de nascimento e o ano atual, e depois calcule a idade da pessoa."""
+
+ano_nascimento = int(input ("infome o seu ano de nasciemnto"))
+ano_atual = int(input ("informe o ano atual"))
+
+print(f'a diferenca de anos e de: {ano_atual - ano_nascimento}')
