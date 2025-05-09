9bbb9e8bcecd76538ea9a0c9e4ddb614bf5f4fe6
tassiapavezi <tassiapacri1@gmail.com>
2025-05-09 10:49:13 -0300

    Add files via upload

---
+++ Comparadores.py
@@ -0,0 +1,19 @@
+#simbulos que comparam valores.
+
+primeiro_numero = int(input("digite o primeiro numero: "))
+segundo_numero = int(input("digite o segundo numero: "))
+# 1 sinal de igual e atribuicoa de valor
+
+#Maior que >10
+print(primeiro_numero > segundo_numero)
+
+#menor que
+print(primeiro_numero < segundo_numero)
+
+#Igual a utiliza o simbilo "==", 2 iguais representa uma comparacao de valores
+
+print(primeiro_numero == segundo_numero)
+
+#Diferente de e representado por "!="
+
+print(primeiro_numero != segundo_numero)
